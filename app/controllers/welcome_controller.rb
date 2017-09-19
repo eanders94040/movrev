2c8a6ad49d2022b82822a295549bd67f9b3be499
eanders94040 <eanders94040@gmail.com>
2017-09-19 15:24:19 -0700

    search by genre

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,6 @@ class WelcomeController < ApplicationController
   end
 
   def create
-    #print 'got here'
     api_results = ModelForMyApi.retrieve_results(params[:email])
 
     @movies = []
@@ -18,14 +17,13 @@ class WelcomeController < ApplicationController
       end
     end
 
-    #print @movies
-
     # sort here
     if params[:submit] == "Sort By Title"
-      #print "sort by title"
       @movies = @movies.sort do |m1,m2| m1.title <=> m2.title end
     elsif params[:submit] == "Sort By Release Date"
       @movies = @movies.sort do |m1,m2| m1.release_date <=> m2.release_date end
+    elsif params[:submit] == "Sort By Genre"
+      @movies = @movies.sort do |m1,m2| m1.genre <=> m2.genre end
     #else
       #print "dont sort"
     end
@@ -39,9 +37,6 @@ class WelcomeController < ApplicationController
     def get_by_external_id_or_create(json)
       movies_in_db = Movie.where("external_id = ?", json['id'])
       if movies_in_db.empty?
-        #genre = get_first_genre(json)
-        #return Movie.create(:title => json['title'], :image => json['poster_path'], :genre => json['genre_ids'], :external_id => json['id'], :release_date => json['release_date'])
-        #return Movie.create(:title => json['title'], :image => json['poster_path'], :genre => genre, :external_id => json['id'], :release_date => json['release_date'])
         return Movie.create(:title => json['title'], :image => json['poster_path'], :genre => json['genre_ids'][0], :external_id => json['id'], :release_date => json['release_date'])
       else
         # should just be one, but take first result if not (todo: ensure just one by db uniqueness constraint)
@@ -50,30 +45,6 @@ class WelcomeController < ApplicationController
         end
       end
     end
-
-    #def get_first_genre(json)
-      #print json
-      #print 'got here\n'
-      #print json['genre_ids']
-      #print json['genre_ids'].class
-      #return json['genre_ids'][0]
-      #genre_arr_json = JSON.parse(json['genre_ids'])  # now i have a json array
-      #print 'after parse'
-      #genre = genre_arr_json[0]
-      #print genre
-      #return genre
-
-      #genre_arr = json['genre_ids'].split(",")
-      #print 'a\n'
-      #print genre_arr
-      #genre = genre_arr[0]
-      #print 'b\n' 
-      #print genre
-      #return genre
-    #end
-
-    #def sort_by_movie_title = ->(m1, m2)
-    #  { m1.title <=> m2.title }
 
     #def get_recent_reviews()
       #recent_reviews = []
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,7 @@
 class Movie < ApplicationRecord
   has_many :reviews
 
-  #def self.get_by_external_id_or_create(movie_json)
-    #movie = Movie.find(:external_id => external_id)
-    #if movie
-    #  return movie
-    #else
-      
-    #end
-  #end
+  enum genre: {action: 28, adventure: 12, animation: 16, comedy: 35, crime: 80, documentary: 99, drama: 18, 
+    family: 10751, fantasy: 14, history: 36, horror: 27, music: 10402, mystery: 9648, romance: 10749, 
+    science_fiction: 878, tv_movie: 10770, thriller: 53, war: 10752, western: 37}
 end
