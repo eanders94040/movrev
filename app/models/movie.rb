1e9c323afa063671e391049495d72cde122d233b
eanders94040 <eanders94040@gmail.com>
2017-09-21 16:34:46 -0700

    average star rating for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,13 @@ class Movie < ApplicationRecord
   enum genre: {action: 28, adventure: 12, animation: 16, comedy: 35, crime: 80, documentary: 99, drama: 18, 
     family: 10751, fantasy: 14, history: 36, horror: 27, music: 10402, mystery: 9648, romance: 10749, 
     science_fiction: 878, tv_movie: 10770, thriller: 53, war: 10752, western: 37, foreign: 10769}
+
+  def average_rating
+    if self.reviews.size > 0
+      self.reviews.average(:rating).round  # todo: partial stars
+    else
+      nil
+    end
+  end
+
 end
