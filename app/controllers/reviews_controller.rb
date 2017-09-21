a1c575e931b3da1ff590ea78cedc4e03acfefa36
eanders94040 <eanders94040@gmail.com>
2017-09-20 18:45:14 -0700

    cleanup

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,16 +14,12 @@ class ReviewsController < ApplicationController
   def create
     movie = Movie.find(params[:movie_id])
     review = movie.reviews.create(review_params)
-    #redirect_to movie_path(@movie)
 
     if review.save
       flash[:success] = "Thanks for creating a review!"
-      #redirect_to movie_path(movie, notice: 'success')
       redirect_to movie_path(movie)
     else
-      #print review.errors.messages
       flash[:error] = review.errors.messages
-      #redirect_to movie_path(movie, notice: 'error')
       redirect_to movie_path(movie)
     end
   end
