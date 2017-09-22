f722be435e83487da31cb6c48ae8c08e3030ec77
eanders94040 <eanders94040@gmail.com>
2017-09-22 10:40:34 -0700

    more detail on movie review pg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def image_full_path(relative_path)
+    return image_full_path_helper(relative_path, "92")
+  end
+
+  def image_full_path_big(relative_path)
+    return image_full_path_helper(relative_path, "185")
+  end
+
+  private
+    def image_full_path_helper(relative_path, size)
+      if relative_path.to_s.empty?
+        return "No image available"
+      else
+        image_tag("http://image.tmdb.org/t/p/w" + size + relative_path)
+      end
+    end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,11 +1,2 @@
 module WelcomeHelper
-
-  def image_full_path(relative_path)
-    if relative_path.to_s.empty?
-      return "No image available"
-    else
-      image_tag("http://image.tmdb.org/t/p/w92" + relative_path)
-    end
-  end
-
 end
